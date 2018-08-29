e0824e7e8daaa3a748198465f03757604729219e
Nikita Skybytsyi <n.skybytskyi@gmail.com>
2018-08-29 20:50:01 +0300

    1028.E

---
+++ codeforces/1028/E.py
@@ -0,0 +1,24 @@
+n = int(input())
+
+b = list(map(int, input().split()))
+
+m, M = min(b), max(b)
+if m == M:
+    if M == 0:
+        print('YES')
+        print(' '.join(['1' for i in range(n)]))
+    else:
+        print('NO')
+else:
+    print('YES')
+
+    pos = list(filter(lambda i: b[i] == M and b[i - 1] < M, range(n)))[0]
+
+    a = [0 for i in range(n)]
+
+    a[pos] = 2 * M
+
+    for i in range(n):
+        a[pos - i] = a[pos - i + 1] + b[pos - i]
+
+    print(*a)
