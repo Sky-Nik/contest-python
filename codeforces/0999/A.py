d97c2fe2df1330ae2a506425a2616f5975173a53
Nikita Skybytsyi <n.skybytskyi@gmail.com>
2019-01-07 09:39:29 +0200

    999.A

---
+++ codeforces/0999/A.py
@@ -0,0 +1,10 @@
+n, k = map(int, input().split())
+a = list(map(int, input().split()))
+ans = 0
+while len(a) > 0 and a[0] <= k:
+  ans += 1
+  a = a[1:]
+while len(a) > 0 and a[-1] <= k:
+  ans += 1
+  a = a[:-1]
+print(ans)
